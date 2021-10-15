e728793afee3ed934fc5f70c8d597ed2691ddf55
HK Transfield <h.k.transfield1@gmail.com>
2021-10-15 16:18:14 +1300

    feat: Complete an implementation of the QKE algorithm

--- Exchange/Simulation.py
+++ Exchange/Simulation.py
@@ -3,31 +3,58 @@
 from Qubit import Qubit
 from numpy import random
 
-random.seed(seed=0)
+# random.seed(seed=0)  # Uncomment if you want to test with the same values every time
 n = 16
 
 
-def exchange():
+def main():
+    """
+    Alice: Transmitter
+    Bob: Receiver
+    """
 
     alice_vals = random.randint(2, size=n)
+    print("Alice's random values: {}".format(alice_vals))
+
     alice_pols = random.randint(2, size=n)
-    alice_message = encode_message(alice_vals, alice_pols)
+    print("Alice's random polarizations: {}".format(alice_pols))
+
+    qubits = encode_qubits(alice_vals, alice_pols)
 
     bob_pols = random.randint(2, size=n)
-    bob_measurements = measure_message(alice_message, bob_pols)
+    print("Bob's random polarizations: {}".format(bob_pols))
+
+    bob_measurements = measure_qubits(qubits, bob_pols)
+    print("Bob's measured values: {}".format(bob_measurements))
+
+    alice_key = generate_key(alice_pols, bob_pols, alice_vals)
+    bob_key = generate_key(alice_pols, bob_pols, bob_measurements)
+
+    if alice_key == bob_key:
+        print("shared key: {}".format(alice_key))
+
+    message_size = 9
+    message = random.randint(n, size=message_size)
 
 
-def encode_message(vals, pols):
-    message = []
+def encode_qubits(vals, pols):
+    """
+    Prepares a stream of qubits to send to a receiver
+    """
+    qubits = []
 
     for i in range(n):
-        qubit = qubit(vals[i], pols[i])
-        message.append(qubit)
+        qubit = Qubit(vals[i], pols[i])
+        qubits.append(qubit)
 
-    return message
+    return qubits
 
 
-def measure_message(message, pols):
+def measure_qubits(message, pols):
+    """
+    Receiver recieves a stream of qubits, and selects a
+    random polarization to measure it
+    """
     measurements = []
 
     for i in range(n):
@@ -36,7 +63,20 @@ def measure_message(message, pols):
 
     return measurements
 
-def generate_key():
+
+def generate_key(a_pols, b_pols, vals):
+    """
+    Forms a secret key by the 
+
+    """
+    key = []
+
+    for i in range(n):
+        if a_pols[i] == b_pols[i]:
+            key.append(vals[i])
+
+    return key
 
 
-#if __name__ == "__main__":
+if __name__ == "__main__":
+    main()
